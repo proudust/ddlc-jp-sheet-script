b34bcb3aadccbd4dbec169a489f0b469c4bba3e2
Proudust <proudust@gmail.com>
2019-07-31 11:06:12 +0900

    :sparkles: Translate インスタンスからスクリプトへの変換を実装

---
+++ src/__test__/converter/toTranslationFile.spec.ts
@@ -0,0 +1,37 @@
+import ToTranslationFile from '../../converter/toTranslationFile';
+import DialogsTranslate from '../../transrate/dialogsTranslate';
+import FileTranslate from '../../transrate/fileTranslate';
+import StringsTranslate from '../../transrate/stringsTranslate';
+import TestResource from '../testResource';
+
+describe(`class ToTranslationFile`, () => {
+  describe(`ToTranslationFile#convert`, () => {
+    it('do', () => {
+      const sheet = [
+        new StringsTranslate('Sayori', 'サヨリ'),
+        new DialogsTranslate('ch0_main_41e273ca', 's', 'Heeeeeeeyyy!!', '「おーはーよーーー！」'),
+        new FileTranslate(
+          'CAN YOU HEAR ME.txt',
+          TestResource.fileContent.orifinal,
+          TestResource.fileContent.translate,
+        ),
+      ];
+      expect(ToTranslationFile.convert('test', sheet)).toStrictEqual([
+        {
+          fileName: 'test.rpy',
+          content: String.raw`translate Japanese ch0_main_41e273ca:
+    s "「おーはーよーーー！」"
+
+translate Japanese strings:
+    old "Sayori"
+    new "サヨリ"
+`,
+        },
+        {
+          fileName: 'CAN YOU HEAR ME.txt',
+          content: TestResource.fileContent.translate,
+        },
+      ]);
+    });
+  });
+});
---
+++ src/converter/toTranslationFile.ts
@@ -0,0 +1,38 @@
+import Translate from '../transrate/translate';
+
+interface File {
+  fileName: string;
+  content: string;
+}
+
+export default {
+  /**
+   * Translate 配列から翻訳スクリプトを出力する
+   */
+  convert: (name: string, translates: Translate[]): File[] => {
+    const defalutFile = { fileName: `${name}.rpy`, content: '' };
+    return translates
+      .sort((a, b) => {
+        if (a.constructor.name < b.constructor.name) {
+          return -1;
+        }
+        if (a.constructor.name > b.constructor.name) {
+          return 1;
+        }
+        return 0;
+      })
+      .reduce<File[]>(
+        (array, curr, index, source) => {
+          const before = index > 0 ? source[index - 1] : null;
+          const output = curr.inflate(before);
+          if (typeof output === 'string') {
+            defalutFile.content += (before ? '\n' : '') + output;
+          } else {
+            array.push(output);
+          }
+          return array;
+        },
+        [defalutFile],
+      );
+  },
+};
