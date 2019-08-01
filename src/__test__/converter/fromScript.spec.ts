7c75401ccd34c4c3d4c762c72d09ba73ada74707
Proudust <proudust@gmail.com>
2019-08-01 13:37:17 +0900

    :sparkles: Ren'Py スクリプトから Translate インスタンスを生成する処理を追加

---
+++ src/__test__/converter/fromScript.spec.ts
@@ -0,0 +1,35 @@
+import FromScript from '../../converter/fromScript';
+import TestResource from '../testResource';
+
+describe(`class FromScript`, () => {
+  describe(`FromScript#convert`, () => {
+    it('do', () => {
+      const sheet = `translate Japanese ch0_main_41e273ca:
+    s "「おーはーよーーー！」"
+
+translate Japanese ch0_main_cb634d94:
+    "やれやれと思いながらサヨリの後について校舎をわたり階段を上っていく。"
+    "着いたのは、学校の中でも普段は３年生の授業や活動で使用され、自分は滅多に行くことがない場所だった。"
+
+translate Japanese strings:
+    old "Sayori"
+    new "サヨリ"
+`;
+      expect(FromScript.convert(sheet)).toMatchObject([
+        {
+          id: 'ch0_main_41e273ca',
+          attribute: 's',
+          original: '',
+          translate: '「おーはーよーーー！」',
+        },
+        {
+          id: 'ch0_main_cb634d94',
+          attribute: '',
+          original: '',
+          translate: TestResource.longDialog.translate,
+        },
+        { id: '', attribute: 'strings', original: 'Sayori', translate: 'サヨリ' },
+      ]);
+    });
+  });
+});
