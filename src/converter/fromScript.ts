71f694df3157885f39a8a6b179ae789f8d740094
Proudust <proudust@gmail.com>
2019-08-02 15:13:15 +0900

    :bug: FromScript を末尾に数値がつくタイプの台詞 ID に対応

--- src/converter/fromScript.ts
+++ src/converter/fromScript.ts
@@ -8,13 +8,16 @@ export default {
    */
   convert: (script: string): Translate[] => {
     const dialogs =
-      script.match(/translate \S+ \S+_[\da-f]{8}:(?:\s+(?:[\d\w]+ ){0,2}"[\s\S]+?")+/g) || [];
+      script.match(/translate \S+ \S+_[\da-f]{8}(?:_\d+)?:(?:\s+(?:[\d\w]+ ){0,2}"[\s\S]+?")+/g) ||
+      [];
     const strings =
       script.match(/old (?:"""\\|")[\s\S]+?(?:"""|").?\s+new (?:"""\\|")[\s\S]+?(?:"""|")/g) || [];
 
     return [
       ...dialogs.map(s => {
-        const props = s.match(/translate \S+ (\S+_[\da-f]{8}):\s+(?:(\w+(?: [\d\w]+)?) )?"/);
+        const props = s.match(
+          /translate \S+ (\S+_[\da-f]{8}(?:_\d+)?):\s+(?:(\w+(?: [\d\w]+)?) )?"/,
+        );
         const dialog = s.match(/"([\s\S]+?)"+/g);
         if (!props || !dialog) throw new Error(`dialog not found. script: ${s}`);
         const translate = dialog.length === 1 ? dialog[0].split('"')[1] : dialog.join('\n');
