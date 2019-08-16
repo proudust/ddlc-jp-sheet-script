3fb44c92413b27531279cd2c167672d6397a13ee
Proudust <proudust@gmail.com>
2019-08-16 11:10:38 +0900

    :bug: MD5 ハッシュにマイナスが入る不具合を修正

--- src/util/md5.ts
+++ src/util/md5.ts
@@ -1,6 +1,7 @@
 const md5 = (string: string): string =>
-  Utilities.computeDigest(Utilities.DigestAlgorithm.MD5, string)
+  Utilities.computeDigest(Utilities.DigestAlgorithm.MD5, string, Utilities.Charset.UTF_8)
     .slice(0, 4)
+    .map(n => (n < 0 ? n + 256 : n))
     .map(n => ('0' + n.toString(16)).slice(-2))
     .join('');
 
