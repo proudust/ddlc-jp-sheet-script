50c6a0b50973f22122bc6514f4ae4380b086d00d
Proudust <proudust@gmail.com>
2019-08-21 00:22:32 +0900

    :bug: 最初の行の翻訳がマージされない不具合を修正

--- src/converter/margeTranslate.ts
+++ src/converter/margeTranslate.ts
@@ -9,7 +9,7 @@ export default {
   marge: (befores: Translate[], afters: Translate[]): Translate[] => {
     return afters.map(after => {
       const matchIndex = firstAtIndex(befores, before => before.original == after.original);
-      return matchIndex ? after.marge(befores.splice(matchIndex, 1)[0]) : after;
+      return matchIndex != null ? after.marge(befores.splice(matchIndex, 1)[0]) : after;
     });
   },
 };
