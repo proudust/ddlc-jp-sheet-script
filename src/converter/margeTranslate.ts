8124a0804043e6d70db5b52288b25b8509ca700f
Proudust <proudust@gmail.com>
2019-08-21 11:46:55 +0900

    :sparkles: スクリプトに見つからない翻訳を、削除タグを付けて適当に差し込むよう変更

--- src/converter/margeTranslate.ts
+++ src/converter/margeTranslate.ts
@@ -1,15 +1,32 @@
 import Translate from '../transrate/translate';
+import IgnoreTranslate from '../transrate/ignoreTranslate';
 
 function firstAtIndex<T>(array: T[], callbackfn: (item: T) => boolean): number | null {
   for (let i = 0; i < array.length; i++) if (callbackfn(array[i])) return i;
   return null;
 }
 
+/**
+ * 任意の Translate インスタンスを IgnoreTranslate インスタンスに変換する
+ * @param before 変換元となる任意の Translate インスタンス
+ */
+function convertIgnoreTranslate(before: Translate): IgnoreTranslate {
+  const { original, translate, comments } = before;
+  return new IgnoreTranslate(original, translate, '削除', comments);
+}
+
 export default {
-  marge: (befores: Translate[], afters: Translate[]): Translate[] => {
-    return afters.map(after => {
+  marge: (sheets: Translate[], scripts: Translate[]): Translate[] => {
+    const befores = [...sheets];
+    return scripts.reduce<Translate[]>((array, after) => {
       const matchIndex = firstAtIndex(befores, before => before.original == after.original);
-      return matchIndex != null ? after.marge(befores.splice(matchIndex, 1)[0]) : after;
-    });
+      const match = matchIndex != null && befores.splice(0, matchIndex + 1);
+      if (match) {
+        const marge = after.marge(match.splice(-1, 1)[0]);
+        const ignores = match.map(convertIgnoreTranslate);
+        array.push(...ignores, marge);
+      } else array.push(after);
+      return array;
+    }, []);
   },
 };
