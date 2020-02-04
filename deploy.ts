e50ba88be737c03eb1d3df677151edab2e9a149d
Proudust <proudust@gmail.com>
2020-02-04 17:28:50 +0900

    :wrench: master に push された時に両方の翻訳所を更新するスクリプトを作成

---
+++ deploy.ts
@@ -0,0 +1,33 @@
+import { exec } from 'child_process';
+import { writeFile } from 'fs';
+import { promisify } from 'util';
+
+interface Dist {
+  name: string;
+  scriptId: string;
+}
+
+const dists: Dist[] = [
+  {
+    name: 'Doki Doki Literature Club! 日本語化 作業所',
+    scriptId: '1zp64PCtW2FYmfDMXn4nFT5g9tA5CWPAaP-qRBg-i0OUb5mjpX1_iOHYr',
+  },
+  {
+    name: 'DDLC Monika After Story 日本語化 作業所',
+    scriptId: '1rKilgJgbw-I_EEMvhYPm17X4BkED0xQkuLo_lVWdM90iXyTOhNi9-rh6',
+  },
+];
+
+const claspOptions = {
+  rootDir: 'dist',
+};
+
+async function push(): Promise<void> {
+  for (const { name, scriptId } of dists) {
+    console.log(`push to "${name}"`);
+    await promisify(writeFile)('.clasp.json', JSON.stringify({ ...claspOptions, scriptId }));
+    await promisify(exec)('clasp push -f');
+  }
+}
+
+push();
