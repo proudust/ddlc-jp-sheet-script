3761fcbb67b2fadee8fce833df14e13d4d200987
Proudust <proudust@gmail.com>
2022-11-06 15:07:21 +0900

    :arrow_up: Migration from mock 0.10.0 to deno_std 0.162.0

--- dev-deps.ts
+++ dev-deps.ts
@@ -3,4 +3,4 @@ export {
   assertEquals,
   assertThrows,
 } from "https://deno.land/std@0.162.0/testing/asserts.ts";
-export { assertSpyCalls, type Stub, stub } from "https://deno.land/x/mock@v0.10.0/mod.ts";
+export { assertSpyCalls, type Stub, stub } from "https://deno.land/std@0.162.0/testing/mock.ts";
