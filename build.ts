769aa44cf7d2eefc2faf65eaeaaba88eaa91f269
Proudust <proudust@gmail.com>
2022-11-06 14:28:37 +0900

    :arrow_up: Bump esbuild from 0.12.15 to 0.15.13

--- build.ts
+++ build.ts
@@ -1,7 +1,7 @@
 import { parse } from "https://deno.land/std@0.162.0/flags/mod.ts";
 import { join } from "https://deno.land/std@0.162.0/path/mod.ts";
 import { colors } from "https://deno.land/x/cliffy@v0.25.4/ansi/mod.ts";
-import { build } from "https://deno.land/x/esbuild@v0.12.15/mod.js";
+import { build } from "https://deno.land/x/esbuild@v0.15.13/mod.js";
 import httpPlugin from "https://deno.land/x/esbuild_plugin_http_fetch@v1.0.2/index.js";
 import { $, cd, ProcessOutput } from "https://deno.land/x/zx_deno@1.2.2/mod.mjs";
 import gasPlugin from "https://esm.sh/esbuild-gas-plugin@0.3.2/mod.ts";
