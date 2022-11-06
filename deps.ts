40ff1017194e9a1c6371aacc02f016df7f9e0bd4
Proudust <proudust@gmail.com>
2022-11-06 15:13:29 +0900

    :arrow_up: Bump zod from 3.11.6 to 3.19.1

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 /// <reference path="https://raw.githubusercontent.com/proudust/deno-gas-types/main/types/index.d.ts" />
 export { takeWhile } from "https://deno.land/std@0.162.0/collections/mod.ts";
-export { z } from "https://deno.land/x/zod@v3.11.6/mod.ts";
+export { z } from "https://deno.land/x/zod@v3.19.1/mod.ts";
 export {
   trimIndent,
   trimMargin,
